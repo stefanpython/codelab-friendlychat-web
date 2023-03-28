3aadfdd4d1cf324a4ba7539af16d332740527c07
stefanpython <stefanhtml@yahoo.com>
2023-03-28 19:00:27 +0300

    initial commit

--- web-start/src/firebase-config.js
+++ web-start/src/firebase-config.js
@@ -1,19 +1,36 @@
-/**
- * To find your Firebase config object:
- * 
- * 1. Go to your [Project settings in the Firebase console](https://console.firebase.google.com/project/_/settings/general/)
- * 2. In the "Your apps" card, select the nickname of the app for which you need a config object.
- * 3. Select Config from the Firebase SDK snippet pane.
- * 4. Copy the config object snippet, then add it here.
- */
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBnS8eiF3pX0U9hL59wE4Ft1YHI59L-HMk",
+  authDomain: "friendlychat-4f0b2.firebaseapp.com",
+  projectId: "friendlychat-4f0b2",
+  storageBucket: "friendlychat-4f0b2.appspot.com",
+  messagingSenderId: "1016651410754",
+  appId: "1:1016651410754:web:1593f5d8994a0a9193ecdb",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 const config = {
-  /* TODO: ADD YOUR FIREBASE CONFIGURATION OBJECT HERE */
+  apiKey: "AIzaSyBnS8eiF3pX0U9hL59wE4Ft1YHI59L-HMk",
+  authDomain: "friendlychat-4f0b2.firebaseapp.com",
+  projectId: "friendlychat-4f0b2",
+  storageBucket: "friendlychat-4f0b2.appspot.com",
+  messagingSenderId: "1016651410754",
+  appId: "1:1016651410754:web:1593f5d8994a0a9193ecdb",
 };
 
 export function getFirebaseConfig() {
   if (!config || !config.apiKey) {
-    throw new Error('No Firebase configuration object provided.' + '\n' +
-    'Add your web app\'s configuration object to firebase-config.js');
+    throw new Error(
+      "No Firebase configuration object provided." +
+        "\n" +
+        "Add your web app's configuration object to firebase-config.js"
+    );
   } else {
     return config;
   }
